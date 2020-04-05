56094ea9133ff31e3f66a05f80dcbcf05498f8cb
gegyhamdani <gegyhamdani@gmail.com>
2020-04-05 21:48:48 +0700

    fetch todo data

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 
 //Todo
 import InputTodo from "./components/Todo/Input";
+import ListTodos from "./components/Todo/List";
 
 function App() {
   return (
     <>
       <div className="container">
         <InputTodo />
+        <ListTodos />
       </div>
     </>
   );
---
+++ src/components/Todo/List.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+
+const ListTodos = () => {
+  const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/todos");
+      const jsonData = await response.json();
+
+      console.log(jsonData);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    let isCancelled = false;
+    if (!isCancelled) getTodos();
+    return () => {
+      isCancelled = true;
+    };
+  }, []);
+
+  return (
+    <>
+      <table className="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            // <tr>
+            //   <td>John</td>
+            //   <td>Doe</td>
+            //   <td>john@example.com</td>
+            // </tr>
+          }
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default ListTodos;
